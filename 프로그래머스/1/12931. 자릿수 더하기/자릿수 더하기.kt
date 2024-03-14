b421bc04ffc4a369e1a959ee96460e3eb282ff79
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-14 16:02:40 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.85 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -2,9 +2,7 @@ class Solution {
     fun solution(n: Int): Int
     {
         var answer: Int= 0
-        n.toString().forEach {
-            answer+=it.digitToInt()
-        }
+        n.toString().forEach { answer+= it.digitToInt() }
         return answer
     }
 }
