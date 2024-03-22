4df40b0fd6e8fb7e8946dbf035b9210dbdbb228f
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-22 17:55:46 +0900

    [level 1] Title: 직사각형 별찍기, Time: 236.33 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,9 +1,4 @@
   fun main(args: Array<String>) {
         val (a, b) = readLine()!!.split(' ').map(String::toInt)
-        for (i in 0 until b) {
-            for (j in 0 until a) {
-                 print('*')
-            }
-            println("")
-        }
+         (1..a*b).forEach { print(if(it%a==0)"*\n" else "*") }
     }
