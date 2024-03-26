20bf33a5ae96e656200ea65f000221a7c1aaf85f
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-26 15:58:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
 
 ### 성능 요약
 
-메모리: 61.9 MB, 시간: 0.02 ms
+메모리: 62.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 15:35:44
+2024년 03월 26일 15:58:57
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,15 +1,4 @@
-import kotlin.math.max
-import kotlin.math.min
-
 class Solution {
-
-    fun solution(n: Int, m: Int)= intArrayOf(gcd(n,m), lcm(n,m))
-
-    fun lcm(n: Int, m: Int)= (n*m)/gcd(n,m)
-    
-    fun gcd(n: Int, m: Int): Int{
-        var r:Int=0;var a= max(n,m); var b= min(n,m)
-        while (b!=0){ r= a%b; a=b; b=r}
-        return a
-    }
+    fun solution(n: Int, m: Int)= intArrayOf(gcd(n,m), n*m/gcd(n,m))
+    private tailrec fun gcd(n: Int, m: Int):Int=if(m<=0) n else gcd(m, n%m)
 }
