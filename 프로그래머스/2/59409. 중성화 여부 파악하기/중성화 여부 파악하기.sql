7a65f3bca3a876c31dcf5b230d15b2737db4eb76
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-27 15:45:38 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, if(SEX_UPON_INTAKE like 'Neutered%' or 
-                           SEX_UPON_INTAKE like 'Spayed%', 'O', 'X')'중성화'
+SELECT ANIMAL_ID, NAME, if(SEX_UPON_INTAKE not like'Intact%', 'O', 'X')'중성화'
 from ANIMAL_INS
-order by ANIMAL_ID 
+order by ANIMAL_ID
