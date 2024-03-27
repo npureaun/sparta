c60e146b890bbb239111a790f6cc0a70d055b25e
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-27 17:59:59 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.05 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -2,10 +2,7 @@ class Solution {
     fun solution(t: String, p: String): Int {
         var answer: Int = 0
         t.forEachIndexed { index, c ->
-            if(index+p.length<=t.length) {
-                val substr = t.substring(index, index + p.length)
-                if (p >= substr) answer++
-            }
+            if(index+p.length<=t.length&& p >= t.substring(index, index + p.length)) answer++
         }
         return answer
     }
