4eca66fb88aeb6e141b57c4522380e2734765ce6
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-28 14:19:30 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,4 @@
 -- 코드를 입력하세요
-SELECT substr(DATETIME, 12, 2)'HOUR', count(*)
+SELECT DATETIME
 from ANIMAL_OUTS
-where HOUR(DATETIME) between '09:00' and '19:59'
-group by substr(DATETIME, 12, 2)
-order by substr(DATETIME, 12, 2)
+where date(between) between '09:00' and '19:59'
