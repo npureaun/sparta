c49708b05151735597489821d5fbd4a23ec1bbc6
npureaun <98468118+npureaun@users.noreply.github.com>
2024-03-29 16:28:16 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 7.03 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -1,16 +1,10 @@
 class Solution {
     fun solution(s: String): Int {
-        var answer: String = ""
+        var answer: String = s
         val strArr=arrayOf("zero", "one","two","three","four","five","six","seven",
             "eight","nine")
-        var Stoken=""
-        s.forEach {
-            if(it.isLetter()) Stoken+=it
-            else answer+=it.digitToInt()
-            if(Stoken.length>=3&&Stoken in strArr) {
-                answer += strArr.indexOf(Stoken)
-                Stoken=""
-            }
+        strArr.forEachIndexed { index, it ->
+            answer=answer.replace(it, index.toString())
         }
         return answer.toInt()
     }
