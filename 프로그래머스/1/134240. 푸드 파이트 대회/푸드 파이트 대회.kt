25520636a5f2b41a6804ebf56f2fcdb1bdd67417
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-04 17:35:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 40.16 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -3,9 +3,9 @@ class Solution {
         var answer: String = ""
         for(i in 1 until food.size)
         {
-            food[i]-=(food[i]%2)
             repeat((food[i]/2)){answer+=i.toString()}
         }
         return answer+"0"+answer.reversed()
     }
 }
+
