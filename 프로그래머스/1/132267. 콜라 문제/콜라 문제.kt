105a13099a9f0e59b1de7a9b43fc8aeaf28967c0
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-08 15:02:18 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -1,16 +1,6 @@
 class Solution {
-    var dp = intArrayOf()
-    tailrec fun solution(a: Int, b: Int, n: Int, answer: Int = 0): Int {
-        dp = IntArray(n) { 0 }
-        return Recursive(a, b, n)
-    }
-
-    tailrec fun Recursive(a: Int, b: Int, n: Int, answer: Int = 0): Int {
+    tailrec fun solution(a: Int, b: Int, n: Int, answer: Int=0): Int {
         if (n < a) return answer
-        else if (dp[(n / a * b) + (n % a)] == 1) return answer
-        else {
-            dp[(n / a * b) + (n % a)] = 1
-            return Recursive(a, b, (n / a * b) + (n % a), answer + (n / a) * b)
-        }
+        return solution(a, b, (n / a * b) + (n % a), answer + (n / a) * b)
     }
 }
