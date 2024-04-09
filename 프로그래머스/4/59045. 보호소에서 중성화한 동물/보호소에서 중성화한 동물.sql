a8fc4ad254e9a82de2772dbf8389306d0c01f192
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-09 15:24:48 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,4 +1,3 @@
 SELECT ao.ANIMAL_ID, ao.ANIMAL_TYPE, ao.NAME
 from ANIMAL_INS ai inner join ANIMAL_OUTS ao using(ANIMAL_ID)
-where ai.SEX_UPON_INTAKE like 'Intact%' and 
-ao.SEX_UPON_OUTCOME not like 'Intact%'
+where ai.SEX_UPON_INTAKE != ao.SEX_UPON_OUTCOME 
