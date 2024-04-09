a8a85b14b96f46099f39d7ac7015f8fb215242d0
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-09 15:05:39 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,4 +1,4 @@
 SELECT ao.ANIMAL_ID, ao.NAME
 FROM ANIMAL_INS ai inner join ANIMAL_OUTS ao using(ANIMAL_ID)
-order by ao.DATETIME-ai.DATETIME desc
+order by datediff(ao.DATETIME,ai.DATETIME) desc
 limit 2
