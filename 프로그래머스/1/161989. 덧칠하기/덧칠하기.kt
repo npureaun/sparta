ab11645bb0a024586fdea8ad34efc5517b19f62e
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-17 11:30:25 +0900

    [level 1] Title: 덧칠하기, Time: 0.79 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -1,17 +1,8 @@
-import java.util.LinkedList
-import java.util.Queue
-
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
         var answer: Int = 0
-        val q: Queue<Int> = LinkedList(section.toList())
-        while (q.isNotEmpty())
-        {
-            val first=q.peek()+m
-            q.poll()
-            while (q.isNotEmpty()&&q.peek()<first) q.poll()
-            answer++
-        }
+        var i=0
+        section.forEach { if(it>=i) {answer++; i=it+m} }
         return answer
     }
 }
