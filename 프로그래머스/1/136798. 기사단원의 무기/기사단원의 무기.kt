4fac699ab5b3c3953f4e06f033b80c0579c3e726
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-17 14:04:06 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.48 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
@@ -1,21 +1,17 @@
+import java.lang.Math.sqrt
+
 class Solution {
-    private fun getDivisorCount(n: Int): Int {
-        if(n==1)return 1
-        else if(n==2||n==3)return 2
+    private fun getCount(n: Int): Int {
         var cnt=0
-        for(i in 1..Math.sqrt(n.toDouble()).toInt())
-        {
-            if(i*i==n) cnt++
-            else if(n%i==0) cnt+=2
-        }
+        for(i in 1..sqrt(n.toDouble()).toInt())
+            if(n%i==0) cnt+=if(i==n/i) 1 else 2
         return cnt
     }
 
     fun solution(number: Int, limit: Int, power: Int): Int {
         var answer = 0
         for (i in 1..number) {
-            val count = getDivisorCount(i)
-            answer += if (count > limit) power else count
+            answer += if (getCount(i) > limit) power else getCount(i)
         }
         return answer
     }
