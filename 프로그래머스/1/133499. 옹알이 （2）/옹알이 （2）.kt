ae2f56ae59ae26d4c5f7f7960f7830dc8e46779f
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-25 15:59:23 +0900

    [level 1] Title: 옹알이 (2), Time: 21.75 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
@@ -1,3 +1,27 @@
 class Solution {
-    fun solution(병신소리: Array<String>) = 병신소리.count { it.matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$".toRegex()) }
+    fun solution(babbling: Array<String>): Int {
+        var answer: Int = 0
+       babbling.forEach {
+           val possible = mutableMapOf("aya" to false, "ye" to false, "woo" to false,  "ma" to false)
+           var str=""
+           var len=0
+           it.forEach { i->
+               str+=i
+               if(possible[str]==false)
+               {
+                   possible.forEach{possible[it.key]=false}
+                   possible[str]=true
+                   len+=str.length
+                   str=""
+               }
+               else if(possible[str]==true)
+               {
+                   possible[str]=false
+                   str=""
+               }
+           }
+           if(len==it.length)answer++
+       }
+        return answer
+    }
 }
