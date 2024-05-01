d9ab5c8ae3fb13e8ad6776df04f0e504e54626e4
npureaun <98468118+npureaun@users.noreply.github.com>
2024-05-01 09:51:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -1,22 +1,13 @@
 class Solution {
     fun solution(ingredient: IntArray): Int {
         var answer: Int = 0
-        val str=""
-        val st= mutableListOf<Int>()
+        val str=StringBuilder()
 
         ingredient.forEach {
-            st.add(it)
-            if(st.size>=4&&it==1) {
-                var mul=1
-                var add=0
-                repeat(4) { cnt ->
-                    add+=mul*st[(st.size-1)-cnt]
-                    mul*=10
-                }
-                if(add==1231) {
-                    repeat(4){st.removeLast()}
-                    answer++
-                }
+            str.append(it)
+            if(str.length>=4&&str.substring(str.length-4)=="1231") {
+                answer++
+                str.setLength(str.length-4)
             }
         }
         return answer
