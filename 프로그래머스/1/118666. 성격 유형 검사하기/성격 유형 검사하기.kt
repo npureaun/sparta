61e5716e253d9a05c9bc85eda69cfd31c43caa8b
npureaun <98468118+npureaun@users.noreply.github.com>
2024-05-08 14:23:50 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 14.34 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(survey: Array<String>, choices: IntArray): String {
+        val map = mutableMapOf("R" to 0, "T" to 0, "C" to 0, "F" to 0,
+            "M" to 0, "J" to 0, "N" to 0, "A" to 0)
+        for(i in choices.indices) {
+            if(choices[i]>4){
+                val key = survey[i].last().toString()
+                map[key]=map.getValue(key)+(choices[i]-4)
+            }
+            else if(choices[i]<4){
+                val key = survey[i].first().toString()
+                map[key]=map.getValue(key)+(4-choices[i])
+            }
+        }
+
+        var answer = ""
+        answer += if(map.getValue("R")>=map.getValue("T")) "R" else "T"
+        answer += if(map.getValue("C")>=map.getValue("F")) "C" else "F"
+        answer += if(map.getValue("J")>=map.getValue("M")) "J" else "M"
+        answer += if(map.getValue("A")>=map.getValue("N")) "A" else "N"
+
+        return answer
+    }
+}
