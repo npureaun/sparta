ff73a87418722d3c271624f2ca8c40194ab06e5b
npureaun <98468118+npureaun@users.noreply.github.com>
2024-06-26 14:42:15 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.60 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -2,13 +2,11 @@ class Solution {
     fun solution(s: String): IntArray {
         var loop=0
         var count=0
-        var str=StringBuilder(s)
-        while (str.toString()!="1"){
+        var str=s
+        while (str!="1"){
             ++loop
-            
             count+= str.count { it.digitToInt() == 0 }
-            str=StringBuilder(str.filter { it.digitToInt() != 0 }
-                .length.toString(2))
+            str=str.filter { it.digitToInt() != 0 }.length.toString(2)
         }
         return  intArrayOf(loop,count)
     }
